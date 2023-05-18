22a23bc27895bc6f911cb2f9d9e37bd097872a65
RAMEEZ RAJA <84043654+ovaid@users.noreply.github.com>
2023-05-18 15:10:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function threeSum(arr, target) {
-// write your code here
-  
+  arr.sort((a, b) => a - b);
+  let closestSum = Infinity;
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+
+    while (left < right) {
+      const currentSum = arr[i] + arr[left] + arr[right];
+      const diff = Math.abs(currentSum - target);
+
+      if (diff < Math.abs(closestSum - target)) {
+        closestSum = currentSum;
+      }
+
+      if (currentSum > target) {
+        right--;
+      } else if (currentSum < target) {
+        left++;
+      } else {
+        return target; // Exact match found
+      }
+    }
+  }
+
+  return closestSum;
 }
+
 
 module.exports = threeSum;
